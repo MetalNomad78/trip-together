8fe565ff6aa4b4607a0a74a298cad8cc1d1f986d
vivek <vivek.yadav@emotorad.in>
2025-07-22 23:15:56 +0530

    Implement user authentication with signup and login functionality

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,24 @@
 const schema = require("../config/schema.json");
 const getTrendingTrips = require("../controllers/getTrendingTripsFromDB.js");
-
+const {
+  userSignup,
+  userLogin,
+} = require("../controllers/loginSignupController.js");
 async function routes(fastify) {
   fastify.post(
     schema.dataBaseApis["getTripsFromDB"].schema.url,
     schema.dataBaseApis["getTripsFromDB"],
     getTrendingTrips,
+  );
+  fastify.post(
+    schema.dataBaseApis["userSignup"].schema.url,
+    schema.dataBaseApis["userSignup"],
+    userSignup,
+  );
+  fastify.post(
+    schema.dataBaseApis["userLogin"].schema.url,
+    schema.dataBaseApis["userLogin"],
+    userLogin,
   );
 }
 
